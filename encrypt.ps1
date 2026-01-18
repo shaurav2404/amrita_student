9337a70a380ed1d440f231a130bfa92deb0d13d7
shaurav2404 <shaurav.shah79@gmail.com>
2026-01-18 23:16:47 +0530

    Update encrypt.ps1

--- encrypt.ps1
+++ encrypt.ps1
@@ -1,7 +1,7 @@
 
-$TenantId     = "280cb568-d1dc-4a34-b94c-06544692d174"
-$ClientId     = "280ab689-1156-4510-8f29-cbfe9566b8ef"
-$ClientSecret = "xgw8Q~Y_AVEt4yDNN3UcCbu4FCyop2b515QuMdr."
+$TenantId     = "280cb568-4692d174"
+$ClientId     = "280ab689-11566b8ef"
+$ClientSecret = "xgw8Q~Y_AVEt4yDNN3Ur."
 
 $TenantId     | ConvertTo-SecureString -AsPlainText -Force | ConvertFrom-SecureString | Set-Content ".\tenantid.enc"
 $ClientId     | ConvertTo-SecureString -AsPlainText -Force | ConvertFrom-SecureString | Set-Content ".\clientid.enc"
