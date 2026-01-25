1b0918dc5498d23313211a9ded0a3b4c364f52fa
Shaurav <shaurav.shah79@gmail.com>
2026-01-25 16:41:09 +0530

    v4

--- PS.ps1
+++ PS.ps1
@@ -1,9 +1,24 @@
-function get-messsage {
+function Test-PSDriveSpace {
     param (
-       # Parameter help description
-       [Parameter(Mandatory=$false)][string]$name 
-           )
-    return "Hello, $name!"
-}
+        [string]$Drive = "C:",
+        [int]$MinFreeGB = 382
+    )
+    $disk = Get-CimInstance Win32_LogicalDisk | 
+            Where-Object { $_.DeviceID -eq $Drive }  #heart of the program
+    if (-not $disk) {
+        Write-Host "Drive $Drive not found."
+        return #exit the function
+    }
+    $freeGB = [math]::Round($disk.FreeSpace / 1GB, 2)
+  
 
-get-messsage 
+
+
+ if ($freeGB -lt $MinFreeGB) {
+        Write-Host "WARNING: Low disk space on $Drive ₹ ₹ Free: $freeGB GB"
+    }
+    else {
+        Write-Host "OK: Disk space on $Drive is healthy -------------Free: $freeGB GB"
+    }
+}
+Test-PSDriveSpace
