c89b19cc1442b8451877701a6f38a01d03894f25
Shaurav <shaurav.shah79@gmail.com>
2026-01-28 23:41:53 +0530

    v7

--- PS.ps1
+++ PS.ps1
@@ -1,25 +1,25 @@
-function Get-ServiceStatus {
+function Restart-ServiceSafe {
     param (
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory = $true)]
         [string]$ServiceName
     )
+
     try {
-     
-           $service = Get-Service -Name $ServiceName -ErrorAction Stop
-    
-    if ($service.Status -eq "Running") 
-    {
-        return "Service '$ServiceName' is running." 
+        $service = Get-Service -Name $ServiceName -ErrorAction Stop
+
+        if ($service.Status -eq 'Running') {
+            Restart-Service -Name $ServiceName -Force
+            return "Service '$ServiceName' was running and has been restarted."
+        }
+        elseif ($service.Status -eq 'Stopped') {
+            Start-Service -Name $ServiceName
+            return "Service '$ServiceName' was stopped and has been started."
+        }
+
+ }
+    catch {
+        return "Service '$ServiceName' does not exist or cannot be accessed."
     }
-    else 
-    {
-        return "Service '$ServiceName' is not running."
-    }
-    }
-    catch 
-    {
-        return "Service '$ServiceName' not found."
-    }
-    
 }
-Get-ServiceStatus
+
+Restart-ServiceSafe
