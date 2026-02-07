6f93f3e180ac0928284d62c00a3257e6e6d25997
Shaurav <shaurav.shah79@gmail.com>
2026-02-07 10:57:26 +0530

    v8

--- PS.ps1
+++ PS.ps1
@@ -1,25 +1,20 @@
-function Restart-ServiceSafe {
+function Move-Files{
     param (
-        [Parameter(Mandatory = $true)]
-        [string]$ServiceName
+    [Parameter(Mandatory= $true)][string]$Filename,
+    [parameter(Mandatory= $true)][string]$Destination,
+    [parameter(Mandatory = $true)][string]$FileType
+ 
     )
-
-    try {
-        $service = Get-Service -Name $ServiceName -ErrorAction Stop
-
-        if ($service.Status -eq 'Running') {
-            Restart-Service -Name $ServiceName -Force
-            return "Service '$ServiceName' was running and has been restarted."
+    if (Test-Path $Filename){
+        Get-ChildItem -path $Filename -Filter "*.$FileType" -ErrorAction Stop |
+        ForEach-Object{
+            Move-Item -Path $_.FullName -Destination $Destination -ErrorAction Stop
         }
-        elseif ($service.Status -eq 'Stopped') {
-            Start-Service -Name $ServiceName
-            return "Service '$ServiceName' was stopped and has been started."
-        }
-
- }
-    catch {
-        return "Service '$ServiceName' does not exist or cannot be accessed."
+        return "File moved successfully to $Destination"
+    }
+    else{
+        return "Failed to move the file."
     }
 }
 
-Restart-ServiceSafe
+Move-Files
